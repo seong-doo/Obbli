c35b5039da990f6e050175e416631a85a2fb3cc9
Shinho Ahn <fracsinus@gmail.com>
2022-01-13 16:05:05 +0900

    server: Add settings for deployment (#42)

--- server/src/controllers/Advert.ts
+++ server/src/controllers/Advert.ts
@@ -18,7 +18,7 @@ const advertList = {
   get: async (req, res): Promise<void> => {
     //게시글 가져오기
     const advertList = await Advert.createQueryBuilder()
-      .select(['title', 'location', 'org.name as org_name', 'active_until'])
+      .select(['Advert.uuid as uuid', 'title', 'location', 'org.name as org_name', 'active_until'])
       .innerJoin('Org', 'org')
       .execute();
     // const advartList = await Advert.find({
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,25 @@
+import cors from 'cors';
 import express from "express";
 import indexRouter from './router'
 import morgan from 'morgan'
 import "reflect-metadata";
 import { createConnection, getConnectionOptions } from "typeorm";
 import {Advert,Application,Org,Org_review,Person,Person_review,Position,Skill } from "./entity";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors({
+  allowedHeaders: ['Authorization', 'Content-Type'],
+  origin: `http://${process.env.APP_DOMAIN}:${port}`,
+  credentials: true,
+  preflightContinue: false,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+}));
 app.use(morgan("dev"));
 
 app.use("/person", indexRouter.Person);
